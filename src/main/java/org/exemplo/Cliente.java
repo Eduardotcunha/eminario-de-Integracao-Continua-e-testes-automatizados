36d800f6bd64fefbcf35b7f97a755f10a90e0c3b
Eduardotcunha <152964398+Eduardotcunha@users.noreply.github.com>
2025-09-30 09:16:23 -0300

    Update Cliente.java teste apre

--- src/main/java/org/exemplo/Cliente.java
+++ src/main/java/org/exemplo/Cliente.java
@@ -3,7 +3,6 @@ class Cliente {
     private int id;
     private String nome;
     private int idade;
-
     public Cliente(int id, String nome, int idade) {
         this.id = id;
         this.nome = nome;
